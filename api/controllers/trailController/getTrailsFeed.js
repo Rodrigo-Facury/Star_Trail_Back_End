462370fafe884cee2b75a0776a477a8846030f68
Rodrigo <rodrigo.facury14@gmail.com>
2023-08-31 15:46:02 -0300

    fix: resolve bug de ordenação

--- api/controllers/trailController/getTrailsFeed.js
+++ api/controllers/trailController/getTrailsFeed.js
@@ -6,52 +6,100 @@ async function getTrailsFeed(req, res, next) {
   const currentPage = req.query.page || 1;
   const orderBy = req.query.orderBy || 'createdAt';
 
+  let trails;
+  let count;
+
   try {
-    const trails = await Trail.findAll({
-      attributes: {
+    if (orderBy === 'starsCount') {
+      const { count: countAll, rows } = await Trail.findAndCountAll({
+        attributes: {
+          include: [
+            [
+              Sequelize.literal(`(
+                SELECT COUNT(*) 
+                FROM "Stars" 
+                WHERE "Stars"."trailId" = "Trail"."id"
+              )`),
+              'starsCount'
+            ]
+          ],
+        },
+        offset: (currentPage - 1) * ITEMS_PER_PAGE,
+        limit: ITEMS_PER_PAGE,
         include: [
-          [
-            Sequelize.literal(`(
-              SELECT COUNT(*) 
-              FROM "Stars" 
-              WHERE "Stars"."trailId" = "Trail"."id"
-            )`),
-            'starsCount'
-          ]
+          {
+            model: Step,
+            as: 'steps',
+            attributes: { exclude: 'trailId' },
+          },
+          {
+            model: User,
+            attributes: ['id', 'username', 'profilePicturePath', 'level'],
+            as: 'creator',
+          },
+          {
+            model: Star,
+            as: 'stars',
+            attributes: ['userId'],
+          },
+          {
+            model: Topic,
+            through: { attributes: [] },
+            attributes: ['name'],
+          }
         ],
-      },
-      offset: (currentPage - 1) * ITEMS_PER_PAGE,
-      limit: ITEMS_PER_PAGE,
-      include: [
-        {
-          model: Step,
-          as: 'steps',
-          attributes: { exclude: 'trailId' },
-        },
-        {
-          model: User,
-          attributes: ['id', 'username', 'profilePicturePath', 'level'],
-          as: 'creator',
-        },
-        {
-          model: Star,
-          as: 'stars',
-          attributes: ['userId'],
-        },
-        {
-          model: Topic,
-          through: { attributes: [] },
-          attributes: ['name'],
-        }
-      ],
-    });
+      });
 
-    const count = trails.length;
+      count = countAll;
+      trails = rows;
+      trails.sort((a, b) => b.dataValues.starsCount - a.dataValues.starsCount);
+    } else {
+      const { count: countAll, rows } = await Trail.findAndCountAll({
+        attributes: {
+          include: [
+            [
+              Sequelize.literal(`(
+                SELECT COUNT(*) 
+                FROM "Stars" 
+                WHERE "Stars"."trailId" = "Trail"."id"
+              )`),
+              'starsCount'
+            ]
+          ],
+        },
+        order: [['createdAt', 'DESC']],
+        offset: (currentPage - 1) * ITEMS_PER_PAGE,
+        limit: ITEMS_PER_PAGE,
+        include: [
+          {
+            model: Step,
+            as: 'steps',
+            attributes: { exclude: 'trailId' },
+          },
+          {
+            model: User,
+            attributes: ['id', 'username', 'profilePicturePath', 'level'],
+            as: 'creator',
+          },
+          {
+            model: Star,
+            as: 'stars',
+            attributes: ['userId'],
+          },
+          {
+            model: Topic,
+            through: { attributes: [] },
+            attributes: ['name'],
+          }
+        ],
+      });
 
-    trails.sort((a, b) => b.dataValues[orderBy] - a.dataValues[orderBy]);
+      count = countAll;
+      trails = rows;
+    }
 
     const totalPages = Math.ceil(count / ITEMS_PER_PAGE);
-
+    
     return res.status(200).json({ trails, currentPage, totalPages, message: 'Feed de trilhas obtido com sucesso!' });
   } catch (err) {
     return next(err);
