4cfe6c5865410b42a68ef10949289213c49a5465
Rodrigo <rodrigo.facury14@gmail.com>
2023-09-11 14:50:42 -0300

    teste

--- api/controllers/userController/getUsersNotifications.js
+++ api/controllers/userController/getUsersNotifications.js
@@ -8,8 +8,6 @@ async function getUsersNotifications(req, res, next) {
       attributes: { exclude: ['password'] }
     });
 
-    console.log('teste')
-
     if (!user) {
       return res.status(404).json({ message: 'Usuário não encontrado!' });
     }
